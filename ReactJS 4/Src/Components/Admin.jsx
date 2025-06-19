45595d9809696bbb5593897264992784768d4fef
Deva muhammad putra <devamuhammad80@gmail.com>
2025-06-20 05:59:37 +0700

    commit keempat

---
+++ ReactJS 4/Src/Components/Admin.jsx
@@ -0,0 +1,4 @@
+const AdminDashboard = () => {
+  return <h2>Dashboard admin</h2>;
+};
+export default AdminDashboard;
--- app.jsx
+++ app.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
-import RegisterForm from './components/RegisterForm';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import AdminDashboard from './components/AdminDashboard';
+import Login from './components/Login';
+import Unauthorized from './components/Unauthorized';
+import ProtectedRoute from './routes/ProtectedRoute';
+import { AuthProvider } from './context/AuthContext';
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <RegisterForm />
-    </div>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/unauthorized" element={<Unauthorized />} />
+
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute allowedRoles={['admin']}>
+                <AdminDashboard />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
-};
+}
 
 export default App;
